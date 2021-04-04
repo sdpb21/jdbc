6b06d86ca043787ea6d36be0cd0c5e86705b4a9e
soygay <jytprf@gmail.com>
2021-04-04 14:41:11 -0400

    Class Statement object created

--- C02InsertDataIntoMysqlDatabase.java
+++ C02InsertDataIntoMysqlDatabase.java
@@ -11,6 +11,8 @@ public class C02InsertDataIntoMysqlDatabase {
 		try {
 			//1. get a connection to database
 			myConnection=DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","K1n9-cr1m50n");
+			//2. create a statement
+			myStatement=myConnection.createStatement();
 		}
 	}
 
