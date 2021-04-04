455256577aee882a440682809a0d13595965c621
soygay <jytprf@gmail.com>
2021-04-04 16:08:00 -0400

    Class Statement object created

--- C03UpdatingDataInAMysqlDatabase.java
+++ C03UpdatingDataInAMysqlDatabase.java
@@ -1,9 +1,11 @@
-import java.sql.Connection;
+import	java.sql.Connection;
+import	java.sql.Statement;
 
 public class C03UpdatingDataInAMysqlDatabase {
 
 	public static void main(String[] args) {
 		Connection myConnection=null;
+		Statement myStatement=null;
 	}
 
 }
