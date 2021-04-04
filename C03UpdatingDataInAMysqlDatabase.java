3fc439b72b2a6a1d2836bb262acc71f5110827d3
soygay <jytprf@gmail.com>
2021-04-04 16:26:25 -0400

    Execute the update query

--- C03UpdatingDataInAMysqlDatabase.java
+++ C03UpdatingDataInAMysqlDatabase.java
@@ -13,6 +13,11 @@ public class C03UpdatingDataInAMysqlDatabase {
 			myConnection=DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","K1n9-cr1m50n");
 			//2. create a statement
 			myStatement=myConnection.createStatement();
+			//3. execute the SQL query
+			String sql="update employees set email='demo@gmail.com' where id=13";
+			int rowsAffected=myStatement.executeUpdate(sql);
+			System.out.println("Rows affected: "+rowsAffected);
+			
 		}
 	}
 
