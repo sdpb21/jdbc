2253c0245c5d9d1d37f6d7ae610ae982b0e433b7
soygay <jytprf@gmail.com>
2021-04-04 16:19:46 -0400

    Create a Statement object

--- C03UpdatingDataInAMysqlDatabase.java
+++ C03UpdatingDataInAMysqlDatabase.java
@@ -11,6 +11,8 @@ public class C03UpdatingDataInAMysqlDatabase {
 		try {
 			//1. get a connection to the database
 			myConnection=DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","K1n9-cr1m50n");
+			//2. create a statement
+			myStatement=myConnection.createStatement();
 		}
 	}
 
