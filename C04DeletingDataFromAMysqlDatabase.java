f9643dfca47498358edaf0a80342fc733351773e
soygay <jytprf@gmail.com>
2021-04-04 20:56:45 -0400

    try block added

--- C04DeletingDataFromAMysqlDatabase.java
+++ C04DeletingDataFromAMysqlDatabase.java
@@ -6,6 +6,10 @@ public class C04DeletingDataFromAMysqlDatabase {
 	public static void main(String[] args) {
 		Connection myConnection=null;
 		Statement myStatement=null;
+
+		try {
+			
+		}
 	}
 
 }
