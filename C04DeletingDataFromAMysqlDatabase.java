16cdeae150be108a212e4a8ea8ddca728308bbb8
soygay <jytprf@gmail.com>
2021-04-04 21:14:49 -0400

    catch section created

--- C04DeletingDataFromAMysqlDatabase.java
+++ C04DeletingDataFromAMysqlDatabase.java
@@ -1,4 +1,5 @@
 import	java.sql.Connection;
+import java.sql.DriverManager;
 import	java.sql.Statement;
 
 public class C04DeletingDataFromAMysqlDatabase {
@@ -7,7 +8,18 @@ public class C04DeletingDataFromAMysqlDatabase {
 		Connection myConnection=null;
 		Statement myStatement=null;
 
-		try {
+		try {//jdbc:mysql://localhost:3306/demo","root","K1n9-cr1m50n
+			//1- get a connection to database
+			myConnection=DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","K1n9-cr1m50n");
+			//2. create a statement
+			myStatement=myConnection.createStatement();
+			//3. execute SQL query
+			String sql="delete from employees where last_name='Brown'";
+			int rowsAffected=myStatement.executeUpdate(sql);
+
+			System.out.println("Rows affected: "+rowsAffected);
+			System.out.println("Delete completed.");
+		}catch(Exception exc) {
 			
 		}
 	}
