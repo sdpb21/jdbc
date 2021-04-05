4e4868c12a6406d9a605ba6b56a1451cf900496a
soygay <jytprf@gmail.com>
2021-04-04 22:13:40 -0400

    Class ResultSet object created

--- C05PreparedStatements.java
+++ C05PreparedStatements.java
@@ -1,11 +1,13 @@
 import	java.sql.Connection;
 import	java.sql.PreparedStatement;
+import	java.sql.ResultSet;
 
 public class C05PreparedStatements {
 
 	public static void main(String[] args) {
 		Connection myConnection=null;
 		PreparedStatement myStatement=null;
+		ResultSet myResultSet=null;
 	}
 
 }
