9b7940e9f74e525596d7485ca5deca0663fba520
soygay <jytprf@gmail.com>
2021-04-04 20:53:43 -0400

    Class Statement object created

--- C04DeletingDataFromAMysqlDatabase.java
+++ C04DeletingDataFromAMysqlDatabase.java
@@ -1,9 +1,11 @@
 import	java.sql.Connection;
+import	java.sql.Statement;
 
 public class C04DeletingDataFromAMysqlDatabase {
 
 	public static void main(String[] args) {
 		Connection myConnection=null;
+		Statement myStatement=null;
 	}
 
 }
