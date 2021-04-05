107c7139f4af4a6ce0d30edd9f175a149133b678
soygay <jytprf@gmail.com>
2021-04-04 22:09:24 -0400

    Class Connection object created

--- C05PreparedStatements.java
+++ C05PreparedStatements.java
@@ -1,9 +1,9 @@
+import	java.sql.Connection;
 
 public class C05PreparedStatements {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Connection myConnection=null;
 	}
 
 }
