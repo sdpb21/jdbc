9768ca5ed2887622e683e987c4478a827e6efd5c
soygay <jytprf@gmail.com>
2021-04-04 22:11:42 -0400

    Class PreparedStatement object created

--- C05PreparedStatements.java
+++ C05PreparedStatements.java
@@ -1,9 +1,11 @@
 import	java.sql.Connection;
+import	java.sql.PreparedStatement;
 
 public class C05PreparedStatements {
 
 	public static void main(String[] args) {
 		Connection myConnection=null;
+		PreparedStatement myStatement=null;
 	}
 
 }
