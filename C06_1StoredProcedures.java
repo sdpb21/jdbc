abb1a3081824b8416879fac1ede5512b54a0da91
soygay <jytprf@gmail.com>
2021-04-05 23:08:03 -0400

    Starting showSalaries method definition

--- C06_1StoredProcedures.java
+++ C06_1StoredProcedures.java
@@ -1,4 +1,5 @@
 import	java.sql.Connection;
+import java.sql.DriverManager;
 import	java.sql.CallableStatement;
 
 public class C06_1StoredProcedures {
@@ -7,9 +8,19 @@ public class C06_1StoredProcedures {
 		Connection myConnection=null;
 		CallableStatement myStatement=null;
 
-		try {
-			
+		try {//"jdbc:mysql://localhost:3306/demo","root","K1n9-cr1m50n"
+			//get a connection to the database
+			myConnection=DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","K1n9-cr1m50n");
+
+			String theDepartment="Engineering";
+			int theIncreaseAmount=10000;
+			//show salaries before
+			System.out.println("Salaries before:");
 		}
+	}
+
+	private static void showSalaries(Connection myCo,String Dep) {
+		
 	}
 
 }
