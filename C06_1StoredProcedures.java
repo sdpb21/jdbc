afe519e235d1613708a61cc7499fd984b42a6ac8
soygay <jytprf@gmail.com>
2021-04-05 22:57:16 -0400

    try section for C06_1 created

--- C06_1StoredProcedures.java
+++ C06_1StoredProcedures.java
@@ -1,8 +1,15 @@
+import	java.sql.Connection;
+import	java.sql.CallableStatement;
 
 public class C06_1StoredProcedures {
 
 	public static void main(String[] args) {
+		Connection myConnection=null;
+		CallableStatement myStatement=null;
 
+		try {
+			
+		}
 	}
 
 }
