6e3eed567726e43b6fd5fe6f54553a19129878b3
soygay <jytprf@gmail.com>
2021-04-06 20:45:50 -0400

    Get a callable statement object from a connection object

--- C06_2StoredProcedures.java
+++ C06_2StoredProcedures.java
@@ -1,8 +1,19 @@
+import	java.sql.Connection;
+import java.sql.DriverManager;
+import	java.sql.CallableStatement;
 
 public class C06_2StoredProcedures {
 
 	public static void main(String[] args) {
+		Connection connectionObject=null;
+		CallableStatement callableStatementObject=null;
 
+		try {//"jdbc:mysql://localhost:3306/demo","root","K1n9-cr1m50n"
+			//get a connection to database
+			connectionObject=DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "root","K1n9-cr1m50n");
+		}catch(Exception exc) {
+			
+		}
 	}
 
 }
