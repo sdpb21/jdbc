63e58edc00d111758eeeefefc0afabdad31d5cd2
soygay <jytprf@gmail.com>
2021-04-07 21:15:01 -0400

    try section added

--- C06_3StoredProcedures.java
+++ C06_3StoredProcedures.java
@@ -6,6 +6,10 @@ public class C06_3StoredProcedures {
 	public static void main(String[] args) {
 		Connection connectionObject=null;
 		CallableStatement callableStatementObject=null;
+
+		try {
+			
+		}
 	}
 
 }
