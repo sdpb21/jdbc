139f55bffdaee2984653cda10ede09c9a5a1753e
soygay <jytprf@gmail.com>
2021-04-08 14:18:56 -0400

    display the result

--- C06_4StoredProcedures_ReturnResultSet.java
+++ C06_4StoredProcedures_ReturnResultSet.java
@@ -2,6 +2,7 @@ import	java.sql.Connection;
 import java.sql.DriverManager;
 import	java.sql.CallableStatement;
 import	java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class C06_4StoredProcedures_ReturnResultSet {
 
@@ -23,8 +24,18 @@ public class C06_4StoredProcedures_ReturnResultSet {
 			System.out.println("Finished calling stored procedure.");
 			//get the result set
 			resultSetObject=callableStatementObject.getResultSet();
-			}catch(Exception exc) {
+			//display the result
+		}catch(Exception exc) {
 			
+		}
+	}
+	private static void display(ResultSet resultSetObject2) throws SQLException {
+		while(resultSetObject2.next()) {
+			String lastName=resultSetObject2.getString("last_name");
+			String firstName=resultSetObject2.getString("first_name");
+			double salary=resultSetObject2.getDouble("salary");
+			String department=resultSetObject2.getString("department");
+			System.out.printf("%s, %s, %s, %.2f\n",lastName,firstName,department,salary);
 		}
 	}
 
