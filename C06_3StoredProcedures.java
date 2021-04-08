fa32de14c08cbde781b25b99aec275adf4c66a44
soygay <jytprf@gmail.com>
2021-04-07 21:54:55 -0400

    get a CallableStatement object for the stored procedure called get_count_for_department

--- C06_3StoredProcedures.java
+++ C06_3StoredProcedures.java
@@ -12,6 +12,8 @@ public class C06_3StoredProcedures {
 			//get a connection to database
 			connectionObject=DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","K1n9-cr1m50n");
 			String theDepartment="Engineering";
+			//prepare the stored procedure call
+			callableStatementObject=connectionObject.prepareCall("{call get_count_for_department(?,?)}");
 		}catch(Exception exc) {
 			
 		}
