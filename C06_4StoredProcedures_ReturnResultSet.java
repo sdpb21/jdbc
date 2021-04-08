297efe42e0237faefc19736315a0402625c746f3
soygay <jytprf@gmail.com>
2021-04-08 13:12:46 -0400

    get a connection object to connect with the database

--- C06_4StoredProcedures_ReturnResultSet.java
+++ C06_4StoredProcedures_ReturnResultSet.java
@@ -1,7 +1,21 @@
+import	java.sql.Connection;
+import java.sql.DriverManager;
+import	java.sql.CallableStatement;
+import	java.sql.ResultSet;
 
 public class C06_4StoredProcedures_ReturnResultSet {
 
 	public static void main(String[] args) {
+		Connection connectionObject=null;
+		CallableStatement callablestatementObject=null;
+		ResultSet resultsetObject=null;
+
+		try {
+			//get a connection to database
+			connectionObject=DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","K1n9-cr1m50n");
+		}catch(Exception exc) {
+			
+		}
 	}
 
 }
