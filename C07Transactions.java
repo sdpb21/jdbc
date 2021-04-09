05aaa69ba70bd0e95a079420a172828beb5f9fb8
soygay <jytprf@gmail.com>
2021-04-08 20:52:45 -0400

    3 parameter class method close created

--- C07Transactions.java
+++ C07Transactions.java
@@ -1,6 +1,8 @@
 import	java.sql.Connection;
 import java.sql.DriverManager;
 import	java.sql.Statement;
+import	java.sql.PreparedStatement;
+import	java.sql.ResultSet;
 
 public class C07Transactions {
 
@@ -18,5 +20,31 @@ public class C07Transactions {
 		}
 	}
 	private static void showSalaries(Connection connectionObject2,String department) {
+		PreparedStatement preparedStatementObject=null;
+		ResultSet resultSetObject=null;
+
+		System.out.println("Show salaries for department: "+department);
+		try {
+			//prepare statement
+			preparedStatementObject=connectionObject2.prepareStatement("select * from employees where department=?");
+			preparedStatementObject.setString(1, department);
+			//execute sql query
+			resultSetObject=preparedStatementObject.executeQuery();
+			//process result set
+			while(resultSetObject.next()) {
+				String lastName=resultSetObject.getString("last_name");
+				String firstName=resultSetObject.getString("first_name");
+				double salary=resultSetObject.getDouble("salary");
+				String department2=resultSetObject.getString("department");
+				System.out.printf("%s, %s, %s, %.2f\n",lastName,firstName,department,salary);
+			}
+		}catch(Exception exc) {
+			exc.printStackTrace();
+		}finally {
+			
+		}
+	}
+	private static void close(Connection connectionObject3,Statement statementObject2,ResultSet resultSetObject) {
+		
 	}
 }
