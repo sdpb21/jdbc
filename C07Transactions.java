299cea04a97db4c1268bba76911d5b2d3430bae6
soygay <jytprf@gmail.com>
2021-04-08 20:21:00 -0400

    get a connection to database object

--- C07Transactions.java
+++ C07Transactions.java
@@ -1,4 +1,5 @@
 import	java.sql.Connection;
+import java.sql.DriverManager;
 import	java.sql.Statement;
 
 public class C07Transactions {
@@ -7,7 +8,8 @@ public class C07Transactions {
 		Connection connectionObject=null;
 		Statement statementObject=null;
 		try {
-			
+			//get a connection to database object
+			connectionObject=DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","K1n9-cr1m50n");
 		}catch(Exception exc) {
 		}
 	}
