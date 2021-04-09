79e0fdaf5a5b4ca99567b2399bdf5e705b368c45
soygay <jytprf@gmail.com>
2021-04-08 20:17:48 -0400

    try catch section added

--- C07Transactions.java
+++ C07Transactions.java
@@ -1,7 +1,15 @@
+import	java.sql.Connection;
+import	java.sql.Statement;
 
 public class C07Transactions {
 
 	public static void main(String[] args) {
+		Connection connectionObject=null;
+		Statement statementObject=null;
+		try {
+			
+		}catch(Exception exc) {
+		}
 	}
 
 }
