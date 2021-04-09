4c54b01f480d11521f339ae0c7e6fb3adc740572
soygay <jytprf@gmail.com>
2021-04-09 12:06:13 -0400

    try catch section added

--- C09ResultSetMetadata.java
+++ C09ResultSetMetadata.java
@@ -1,7 +1,17 @@
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 public class C09ResultSetMetadata {
 
 	public static void main(String[] args) {
+		Connection connectionObject=null;
+		Statement statementObject=null;
+		ResultSet resultSetObject=null;
+
+		try {
+		}catch(Exception exc) {
+		}
 	}
 
 }
