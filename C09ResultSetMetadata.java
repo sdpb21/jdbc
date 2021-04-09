2a18b2669348037c90446e521546184a0ea2e8fa
soygay <jytprf@gmail.com>
2021-04-09 12:45:11 -0400

    show result set object metadata information

--- C09ResultSetMetadata.java
+++ C09ResultSetMetadata.java
@@ -19,6 +19,15 @@ public class C09ResultSetMetadata {
 			resultSetObject=statementObject.executeQuery("select id,last_name,first_name,salary from employees");
 			//get result set metadata
 			ResultSetMetaData resultSetMetaDataObject=resultSetObject.getMetaData();
+			//display info
+			int columnCount=resultSetMetaDataObject.getColumnCount();
+			System.out.println("Column count: "+columnCount+"\n");
+			for(int column=1;column<=columnCount;column++) {
+				System.out.println("Column name: "+resultSetMetaDataObject.getColumnName(column));
+				System.out.println("Column type name: "+resultSetMetaDataObject.getColumnTypeName(column));
+				System.out.println("Is nullable: "+resultSetMetaDataObject.isNullable(column));
+				System.out.println("Is auto increment: "+resultSetMetaDataObject.isAutoIncrement(column)+"\n");
+			}
 		}catch(Exception exc) {
 		}
 	}
