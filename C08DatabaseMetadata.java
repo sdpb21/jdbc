2d715dc1615a803b933f20eecfb2aba4da9bc4c2
soygay <jytprf@gmail.com>
2021-04-09 08:45:59 -0400

    display information about the database

--- C08DatabaseMetadata.java
+++ C08DatabaseMetadata.java
@@ -12,6 +12,10 @@ public class C08DatabaseMetadata {
 			connectionObject=DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","K1n9-cr1m50n");
 			//get metadata
 			DatabaseMetaData databaseMetadataObject=connectionObject.getMetaData();
+			//display information about database
+			System.out.println("Product name: "+databaseMetadataObject.getDatabaseProductName());
+			System.out.println("Product version: "+databaseMetadataObject.getDatabaseProductVersion());
+			System.out.printf("\n");
 		}catch(Exception exc) {
 		}
 	}
