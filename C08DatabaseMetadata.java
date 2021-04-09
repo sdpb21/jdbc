5f51ea0a1f754eb2d6a76850e1655369df8c8ad4
soygay <jytprf@gmail.com>
2021-04-09 08:24:03 -0400

    try and catch section created

--- C08DatabaseMetadata.java
+++ C08DatabaseMetadata.java
@@ -1,7 +1,13 @@
+import	java.sql.Connection;
 
 public class C08DatabaseMetadata {
 
 	public static void main(String[] args) {
+		Connection connectionObject=null;
+
+		try {
+		}catch(Exception exc) {
+		}
 	}
 
 }
