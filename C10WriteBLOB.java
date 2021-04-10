d208db342f555a67c3804d1dcb8a09431dcfc05c
soygay <jytprf@gmail.com>
2021-04-09 22:41:56 -0400

    set parameter for the file name

--- C10WriteBLOB.java
+++ C10WriteBLOB.java
@@ -1,7 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.io.File;
+import java.io.FileInputStream;
 
 public class C10WriteBLOB {
 
 	public static void main(String[] args) {
+		Connection connectionObject=null;
+		PreparedStatement preparedStatementObject=null;
+		FileInputStream input=null;
+
+		try {
+			// get a connection object to database
+			connectionObject=DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","K1n9-cr1m50n");
+			//prepare statement
+			String sql="update employees set resume=? where email='john.doe@foo.com'";
+			preparedStatementObject=connectionObject.prepareStatement(sql);
+			//set parameter for resume file name
+			File theFile=new File("sample_resume.pdf");
+			input=new FileInputStream(theFile);
+			preparedStatementObject.setBinaryStream(1, input);
+			System.out.println("Reading input file: "+theFile.getAbsolutePath());
+		}catch(Exception exc) {
+		}
 	}
 
 }
