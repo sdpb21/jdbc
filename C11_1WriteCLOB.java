7251e7fd65d662c49fbf8b1472cd16c1596b49a4
soygay <jytprf@gmail.com>
2021-04-11 22:59:02 -0400

    creating a new FileReader object given the file to read (fileObject)

--- C11_1WriteCLOB.java
+++ C11_1WriteCLOB.java
@@ -1,7 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.io.FileReader;
+import java.io.File;
 
 public class C11_1WriteCLOB {
 
 	public static void main(String[] args) {
+		Connection connectionObject=null;
+		PreparedStatement preparedStatementObject=null;
+		FileReader inputFileReaderObject=null;
+
+		try {
+			//get a connection to database
+			connectionObject=DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","K1n9-cr1m50n");
+			//prepare Statement
+			String sql="update employees set resume=? where email='john.doe@foo.com'";
+			preparedStatementObject=connectionObject.prepareStatement(sql);
+			//set parameter for document file name
+			File fileObject=new File("sample_resume.txt");
+			inputFileReaderObject=new FileReader(fileObject);
+		}catch(Exception exc) {
+		}
 	}
 
 }
