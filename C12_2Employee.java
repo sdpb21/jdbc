978802fba683eff56713b28117e607a5fb893367
soygay <jytprf@gmail.com>
2021-04-13 22:32:21 -0400

    Class Employee constructor completed

--- C12_2Employee.java
+++ C12_2Employee.java
@@ -1,3 +1,18 @@
+import java.math.BigDecimal;
 
 public class C12_2Employee {
+	private int id;
+	private String lastName;
+	private String firstName;
+	private String email;
+	private BigDecimal salary;
+	//constructor
+	public C12_2Employee(int id2,String lastName2,String firstName2,String email2,BigDecimal salary2) {
+		super();
+		id=id2;
+		lastName=lastName2;
+		firstName=firstName2;
+		email=email2;
+		salary=salary2;
+	}
 }
